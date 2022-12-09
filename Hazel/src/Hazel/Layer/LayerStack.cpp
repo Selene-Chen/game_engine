97b128320bbd1e609b54e5d6d40b463b62a10107
Selene <339698418@qq.com>
2022-12-10 03:33:10 +0800

    fixed:释放层栈，每个层应该都OnDetach之后在释放

--- Hazel/src/Hazel/Layer/LayerStack.cpp
+++ Hazel/src/Hazel/Layer/LayerStack.cpp
@@ -5,8 +5,11 @@ namespace Hazel
 
     LayerStack::~LayerStack()
     {
-        for (Layer* layer : m_Layers)
+        for (Layer* layer : m_Layers) {
+            layer->OnDetach();
             delete layer;
+        }
+           
     }
 
     void LayerStack::PushLayer(Layer* layer)
